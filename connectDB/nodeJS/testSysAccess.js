9a403e9473fa0e1448ddbb9e11b5581c9d708857
KimGyuPyo <deity@gmail.com>
2012-08-07 17:44:02 +0900

    all delete

--- connectDB/nodeJS/testSysAccess.js
+++
@@ -1,9 +0,0 @@
-var sys = require("sys");
-	spwan = require("child_process").spawn;
-	
-var ls = spwan("ls",["-ls","/"]);
-
-ls.stdout.addListener("data",function(){
-	sys.print(data);
-});
-	
--- connectDB/nodeJS/testTCP.js
+++
@@ -1,9 +0,0 @@
-var tcp = require("net");
-
-var s = tcp.createServer();
-s.addListener("connection",function(c){
-	c.write("hello!");
-	c.end();
-});
-
-s.listen(8000);
